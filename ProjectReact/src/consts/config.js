9e896886997a77247d0af785589a6070ab5ee6d3
tiagopt79 <1240883@isep.ipp.pt>
2026-01-03 00:33:11 +0000

    elem de uma pasta

--- ProjectReact/src/consts/config.js
+++
@@ -1,4 +0,0 @@
-export const RESTDB_CONFIG = {
-  API_KEY: '55832e3e87b83972153b7a8de3350452f3560',  // ← Substitui pela tua API Key do RestDB
-  DATABASE_URL: 'https://mybookshelf-49a2.restdb.io/rest',  // ← Substitui pelo teu URL
-};
